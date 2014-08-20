b35d1bf7b9e21483533dac4c183163d3645aca0f
Baxter Ross <baxterross@gmail.com>
2014-08-20 03:04:21 -0700

    Adding new routes and assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Cgdesignbuild::Application.routes.draw do
   root 'static#index'
+
+  get 'services' => 'services#index'
+  get 'services/design' => 'services#design'
+  get 'services/repositioning' => 'services#repositioning'
+
+  get 'built' => 'built#index'
+  get 'built/sea-colony' => 'built#sea_colony'
 end
