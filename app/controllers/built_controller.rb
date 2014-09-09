0577d6c5f6e665f062b5d6b1b8a6ce7dc16cd220
Baxter Ross <baxterross@gmail.com>
2014-09-08 17:59:34 -0700

    tweaking build work page footer

--- app/controllers/built_controller.rb
+++ app/controllers/built_controller.rb
@@ -12,15 +12,14 @@ class BuiltController < ApplicationController
                     Addressing thhe issue of natural light and privacy, the design utilizes existing and new skylights and an extensive low-lighting system to illuminate the minimalist space.
                     The pristine whites and greys of concrete floors, white walls. and an effortless steel staircase act to channel the ocean air and sunlight into the home',
       images: [
-        'sea-colony/01.jpg',
-        'sea-colony/02.jpg',
-        'sea-colony/03.jpg',
-        'sea-colony/04.jpg',
-        'sea-colony/05.jpg',
-        'sea-colony/06.jpg',
-        'sea-colony/07.jpg',
-        'sea-colony/08.jpg',
-        'sea-colony/09.jpg'
+        'sea-colony/1.jpg',
+        'sea-colony/2.jpg',
+        'sea-colony/3.jpg',
+        'sea-colony/4.jpg',
+        'sea-colony/5.jpg',
+        'sea-colony/6.jpg',
+        'sea-colony/7.jpg',
+        'sea-colony/8.jpg'
       ]
     }
     render 'static/built'
