106013a4a371cba2e428fc468545d5ed2e227e55
Baxter Ross <baxterross@gmail.com>
2014-09-22 13:50:27 -0700

    revising controller for new slideshow

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,16 @@
 class StaticController < ApplicationController
 
   def index
+    @images = [
+      'sea-colony/1.jpg',
+      'sea-colony/2.jpg',
+      'sea-colony/3.jpg',
+      'sea-colony/4.jpg',
+      'sea-colony/5.jpg',
+      'sea-colony/6.jpg',
+      'sea-colony/7.jpg',
+      'sea-colony/8.jpg'
+    ]
   end
   
   def about
