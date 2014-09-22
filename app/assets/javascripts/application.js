8a5e6a1f94cabf1f64264c76a55773f0f5796a35
Baxter Ross <baxterross@gmail.com>
2014-09-22 14:03:20 -0700

    moving jquery to ja folder and loading GA in the application.haml file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require ga
 //= require jquery.2.1.1.min
 //= require cg
 //= require contactform
